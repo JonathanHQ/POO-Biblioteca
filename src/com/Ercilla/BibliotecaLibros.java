ff61573dfed647f84e8adf4e396d3bb0d2be3cff
jonathan <jonalex2984@gmail.com>
2026-02-10 13:25:46 +0100

    Termina implementacion de la clase main

--- src/com/Ercilla/BibliotecaLibros.java
+++ src/com/Ercilla/BibliotecaLibros.java
@@ -2,6 +2,18 @@ package com.Ercilla;
 
 public class BibliotecaLibros {
     public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
 
+        biblioteca.agregarLibro(new Terror("Stephen King","It", 9));
+        biblioteca.agregarLibro(new CienciaFiccion("Asimov","Fundacion","Trantor"));
+        biblioteca.agregarLibro(new NovelaNegra("Agata Cristie","Diez negritos","Wargrave"));
+        biblioteca.agregarLibro(new NovelaRomantica("Jane Austen","Orgullo y Prejuicio",true));
+
+        biblioteca.mostrarLibros();
+
+        System.out.println("________Prestamos________");
+        biblioteca.prestarLibros();
+        System.out.println("----------Prestar otra vez__________");
+        biblioteca.prestarLibros();
     }
 }
